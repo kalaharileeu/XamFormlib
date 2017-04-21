9e720a6ebab69f9e66a7f8cedf18b6e6aac99aad
Cornel <cfdelange@gmail.com>
2017-04-21 20:21:39 +1200

    This works on the emulator, this is the defaulted created project with library project

---
+++ XamAppTest/XamAppTest/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace XamAppTest.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
